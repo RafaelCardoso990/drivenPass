853616b6f2e9a21bc25bbcf7ab266cc20e6c1b48
isabellacgmsa <isabellacgmsa1@gmail.com>
2022-08-04 18:28:14 -0300

    Preparando revisão de código

--- src/schemas/notesSchemas.ts
+++
@@ -1,7 +0,0 @@
-import joi from "joi"
-import { notesTypeData } from "../services/notesService.js"
-
-export const createNotes = joi.object<notesTypeData>({
-    title: joi.string().required().max(50),
-    annotation: joi.string().required().max(1000),
-})
